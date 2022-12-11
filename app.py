f98ee6894e4e834ee530c045f39be2f8541355b5
yoy159 <yoy159@naver.com>
2022-12-12 04:54:52 +0900

    페이지 초기 설정

--- app.py
+++ app.py
@@ -358,6 +358,21 @@ def bucket_post():
 
     return jsonify({'msg': '등록완료'})
 
+@app.route('/partner_list')
+def partner_list():
+    return render_template("partner_list.html")
+
+@app.route('/menu_list')
+def menu_list():
+    return render_template("menu_list.html")
+
+@app.route('/order_list')
+def order_list():
+    return render_template("order_list.html")
+
+@app.route('/price_setting')
+def price_setting():
+    return render_template("price_setting.html")
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
